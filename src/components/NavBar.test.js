0ee428622cf7c77b85959729a48bceae40bf112e
sebalp100 <sebalp100@gmail.com>
2022-12-08 11:44:05 -0300

    Add initial nav bar test

---
+++ src/components/NavBar.test.js
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import React from 'react'
+import '@testing-library/jest-dom'
+import { BrowserRouter, MemoryRouter } from 'react-router-dom'
+import Nav from './NavBar'
+
+
+test('full app rendering/navigating', async () => {
+  render(<Nav />, { wrapper: BrowserRouter })
+  const user = userEvent.setup()
+
+  // verify page content for default route
+  expect(screen.getByText(/Home/i)).toBeInTheDocument()
+
+  // verify page content for expected route after navigating
+  await user.click(screen.getByText(/quote/i))
+  expect(screen.getByText(/Albert/i)).toBeInTheDocument()
+})
